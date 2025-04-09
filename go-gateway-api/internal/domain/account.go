1dd5731578ad343504d4431607afdd43715a48c1
Antonio Malato <malatoneto@gmail.com>
2025-04-08 21:55:24 -0300

    add accounts and services  account

---
+++ go-gateway-api/internal/domain/account.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Account struct {
+	ID        string
+	Name      string
+	Email     string
+	APIKey    string
+	Balance   float64
+	mu        sync.RWMutex
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func generateAPIKEY() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
+
+func NewAccount(name, email, apiKey string) *Account {
+	account := &Account{
+		ID:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		Balance:   0,
+		APIKey:    generateAPIKEY(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	return account
+}
+
+func (a *Account) AddBalance(amount float64) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.Balance += amount
+	a.UpdatedAt = time.Now()
+}
